7d15ce870551148d0c72203e7ac4e0c8bfa8ef2d
Trevor Leach <tleachman1234@gmail.com>
2017-01-10 22:22:39 -0600

    Transferring over to React pt1
    This is the first half of the React transfer.
    As of this commit, the landing page works. You can go to the register page and the base html page is viewable
    No slack data is integrated yet, so the base page holds no content.
    
    I'm having an issue where the base page's width is only half that of the screen. Stuck on why right now.
    
    See DummyFile.jsx to learn the React syntax

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+var config = {
+   entry: './static/main.js',
+	
+   output: {
+      path:'./',
+      filename: 'index.js',
+   },
+	
+   devServer: {
+      inline: true,
+      port: 5000
+   },
+	
+   module: {
+      loaders: [
+         {
+            test: /\.jsx?$/,
+            exclude: /node_modules/,
+            loader: 'babel',
+				
+            query: {
+               presets: ['es2015', 'react']
+            }
+         }
+      ]
+   }
+}
+
+module.exports = config;
