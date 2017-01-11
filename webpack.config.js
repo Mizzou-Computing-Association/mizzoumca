3bbcbe2a73c61e31ae5592a7da4b99c700c11522
Trevor Leach <tleachman1234@gmail.com>
2017-01-10 23:28:04 -0600

    README update and config changes
    Updated the README with information about the React version.
    Modified some config options for optimization and later use.

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,14 @@
+path = require('path');
+const webpack = require('webpack');
+
 var config = {
+   devtool: 'cheap-module-source-map',
+
    entry: './static/main.js',
 	
    output: {
       path:'./',
-      filename: 'index.js',
+      filename: 'index.js'
    },
 	
    devServer: {
@@ -23,7 +28,15 @@ var config = {
             }
          }
       ]
-   }
+   },
+
+   plugins: [
+      new webpack.DefinePlugin({
+         'process.env': {
+            'NODE_ENV': JSON.stringify('production')
+         }
+      })
+   ]
 }
 
 module.exports = config;
