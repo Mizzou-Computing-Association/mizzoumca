b8a26978ac07ce8366b2695e182ca8c3a5666509
Luke Guerdan <lguerdan@yahoo.com>
2017-02-04 16:28:18 -0600

    change home

--- components/home.jsx
+++ components/home.jsx
@@ -2,13 +2,33 @@ import React from 'react';
 
 class Home extends React.Component {
 	render() {
-		return (
-			<ul>
-			  {% for message in updates['general'] %}
-			    <li><h5>Update: {{ message['text']|safe }}</h5><p>Posted at: {{message['ts']}}</p></li>
-			  {% endfor %}
-		  </ul>
-		);
+		let updates = {
+			'general': [
+				{
+					'text': 'test1',
+					'ts': 100,
+				},
+				{
+					'text': 'test2',
+					'ts': 200,
+				}
+			]
+		};
+
+		let slackFeed = updates['general'].map(function(currEl) {
+			return (
+				<ul>
+				    <li>
+				    	<h5>
+				    		Update: {currEl['text']}
+				    	</h5>
+				    	<p>Posted at: {currEl['ts']}</p>
+				    </li>
+			  </ul>
+			)
+		}, this);
+
+		return <div>{slackFeed}</div>;
 	}
 }
 
