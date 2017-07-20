86c4471de53a253429568c7acdc4de5b1652f218
lguerdan <lguerdan@yahoo.com>
2017-07-19 21:48:58 -0700

    effects in landing section

--- static/script.js
+++ static/script.js
@@ -19,6 +19,13 @@ $(document).ready(function() {
       draggable: true // Choose whether you can drag to open on touch screens
     }
   );
+   jQuery(document).ready(function(){
+   $('.header-main').mousemove(function(e){
+      var rXP = (e.pageX - this.offsetLeft-$(this).width()/2);
+      var rYP = (e.pageY - this.offsetTop-$(this).height()/2);
+      $('.header-main').css('text-shadow', +rYP/15+'px '+rXP/120+'px rgba( 255, 204, 188,1), '+rYP/12+'px '+rXP/90+'px rgba(164, 177, 211,1), '+rXP/105+'px '+rYP/18+'px rgba(178, 235, 242,1)');
+      });
+   });
 
 });
 
